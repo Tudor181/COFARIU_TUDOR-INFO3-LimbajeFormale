06e6afb070fe13fb41234258aba3507498664fc9
Tudor-Cofariu <tudor.alin45@gmail.com>
2025-04-06 21:15:21 +0300

    Lexer - ep 1

---
+++ Proiect/ShelbyLanguage/basic.py
@@ -0,0 +1,176 @@
+#######################################
+# CONSTANTS
+#######################################
+
+DIGITS = "0123456789"
+
+#######################################
+# ERRORS
+#######################################
+
+
+class Error:
+    """Class for defining error."""
+
+    def __init__(self, pos_start, pos_end, error_name, details):
+        self.pos_start = pos_start
+        self.pos_end = pos_end
+        self.error_name = error_name
+        self.details = details
+
+    def as_string(self):
+        result = f"{self.error_name}: {self.details}\n"
+        result += f"File {self.pos_start.file_name}, line {self.pos_start.ln + 1}"
+        return result
+
+
+class IllegalCharError(Error):
+    def __init__(self, pos_start, pos_end, details):
+        super().__init__(pos_start, pos_end, "Illegal Character", details)
+
+
+#######################################
+# POSITION
+#######################################
+
+
+class Position:
+    def __init__(self, index, ln, col, file_name, file_text):
+        self.index = index
+        self.ln = ln
+        self.col = col
+        self.file_name = file_name
+        self.file_text = file_text
+
+    def advance(self, current_char):
+        self.index += 1
+        self.col += 1
+
+        if current_char == "\n":
+            self.ln += 1
+            self.col = 0
+
+        return self
+
+    def copy(self):
+        return Position(self.index, self.ln, self.col, self.file_name, self.file_text)
+
+
+#######################################
+# TOKENS
+#######################################
+## TT - token type
+##@check - poate putem adauga mai multe tipuri de tokens verifica 3.3 din pdf
+TT_INT = "INT"
+TT_FLOAT = "FLOAT"
+TT_PLUS = "PLUS"
+TT_MINUS = "MINUS"
+TT_MUL = "MUL"
+TT_DIV = "DIV"
+TT_LPAREN = "LPAREN"
+TT_RPAREN = "RPAREN"
+
+
+class Token:
+    """Token."""
+
+    def __init__(self, type_, value=None):
+        self.type = type_
+        self.value = value
+
+    def __repr__(self):
+        """Returnează o reprezentare ușor de citit a obiectului Token,
+        folosită automat de funcția print() și consola Python.
+        Exemplu: Token(TT_INT, 5) -> 'INT:5'"""
+
+        if self.value:
+            return f"{self.type}:{self.value}"
+        return f"{self.type}"
+
+
+#######################################
+# LEXER
+#######################################
+
+
+class Lexer:
+    def __init__(self, file_name, text):
+        self.file_name = file_name
+        self.text = text
+        self.pos = Position(index=-1, ln=0, col=-1, file_name=file_name, file_text=text)
+        self.current_char = None
+        self.advance()
+
+    def advance(self):
+        self.pos.advance(self.current_char)
+        self.current_char = (
+            self.text[self.pos.index] if self.pos.index < len(self.text) else None
+        )
+
+    def make_tokens(self):
+        tokens = []
+
+        while self.current_char != None:
+            if self.current_char in " \t":
+                self.advance()
+            elif self.current_char in DIGITS:
+                tokens.append(self.make_number())
+            elif self.current_char == "+":
+                tokens.append(Token(TT_PLUS))
+                self.advance()
+            elif self.current_char == "-":
+                tokens.append(Token(TT_MINUS))
+                self.advance()
+            elif self.current_char == "*":
+                tokens.append(Token(TT_MUL))
+                self.advance()
+            elif self.current_char == "/":
+                tokens.append(Token(TT_DIV))
+                self.advance()
+            elif self.current_char == "(":
+                tokens.append(Token(TT_LPAREN))
+                self.advance()
+            elif self.current_char == ")":
+                tokens.append(Token(TT_RPAREN))
+                self.advance()
+            else:
+                pos_start = self.pos.copy()
+                char = self.current_char
+                self.advance()
+                return [], IllegalCharError(
+                    pos_start, pos_end=self.pos, details="'" + char + "'"
+                )
+
+        return tokens, None
+
+    def make_number(self):
+        num_str = ""
+        dot_count = 0
+
+        while self.current_char != None and self.current_char in DIGITS + ".":
+            if self.current_char == ".":
+                if dot_count == 1:
+                    break
+                dot_count += 1
+                num_str += "."
+            else:
+                num_str += self.current_char
+            self.advance()
+
+        if dot_count == 0:
+            return Token(TT_INT, value=int(num_str))
+        else:
+            return Token(TT_FLOAT, value=float(num_str))
+
+
+#######################################
+# RUN
+#######################################
+
+
+def run(file_name, text):
+    """Function running the Shelby Language"""
+    lexer = Lexer(file_name, text)
+    tokens, error = lexer.make_tokens()
+
+    return tokens, error
